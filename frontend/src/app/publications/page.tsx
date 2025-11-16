83c599d4c2f9df8a15167c20a2d4612377259181
hakolr <m.gayvoronskiy@flang.io>
2025-11-16 14:30:20 +0300

    fix

--- frontend/src/app/publications/page.tsx
+++ frontend/src/app/publications/page.tsx
@@ -2,32 +2,28 @@ import PublicationsPage from '@/screens/PublicationsPage'
 import { dehydrate } from '@tanstack/react-query'
 import { HydrationBoundary } from '@tanstack/react-query'
 import { prefetchPublicationsPage } from './prefetch'
-import {
-  PublicationsController,
-  PublicationsListQueryParams,
-  mapPublicationParams,
-} from '@/entities/publication'
+import { PublicationsListQueryParams } from '@/entities/publication'
 
 type PageProps = {
   searchParams: Promise<PublicationsListQueryParams>
 }
 
-export async function generateMetadata({ searchParams }: PageProps) {
-  const params = await searchParams
-  const publications = await PublicationsController.getPublications(
-    mapPublicationParams(params),
-  )
+// export async function generateMetadata({ searchParams }: PageProps) {
+//   const params = await searchParams
+//   const publications = await PublicationsController.getPublications(
+//     mapPublicationParams(params),
+//   )
 
-  const publicationsTitles = publications.data.map(
-    (publication) => publication.title,
-  )
+//   const publicationsTitles = publications.data.map(
+//     (publication) => publication.title,
+//   )
 
-  return {
-    title: 'Публикации',
-    description: 'Публикации',
-    keywords: publicationsTitles.join(', '),
-  }
-}
+//   return {
+//     title: 'Публикации',
+//     description: 'Публикации',
+//     keywords: publicationsTitles.join(', '),
+//   }
+// }
 
 export default async function Page({ searchParams }: PageProps) {
   const queryClient = await prefetchPublicationsPage(await searchParams)
